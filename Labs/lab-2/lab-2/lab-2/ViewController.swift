0eb4e641bc909bdf6955e1c1e62015e516c50d25
Lucas Dachman <lucas.dachman@gmail.com>
2018-09-19 12:30:42 -0600

    autolayout test

---
+++ Labs/lab-2/lab-2/lab-2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lab-2
+//
+//  Created by Lucas Dachman on 9/19/18.
+//  Copyright © 2018 Lucas Dachman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

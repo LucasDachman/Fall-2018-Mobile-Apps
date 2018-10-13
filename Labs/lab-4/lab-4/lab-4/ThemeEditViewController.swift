c5f3f492cd49930cc80dffade92a2488bf596105
Lucas Dachman <lucas.dachman@gmail.com>
2018-10-12 19:11:29 -0600

    add functionality for multiple picker views

--- Labs/lab-4/lab-4/lab-4/ThemeEditViewController.swift
+++ Labs/lab-4/lab-4/lab-4/ThemeEditViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ThemeEditViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+class ThemeEditViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UITextViewDelegate {
     @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var textColorPicker: UIPickerView!
     @IBOutlet weak var textSizePicker: UIPickerView!
@@ -20,17 +20,52 @@ class ThemeEditViewController: UIViewController, UIPickerViewDelegate, UIPickerV
                    (color: UIColor.blue, name: "blue"),
                    (color: UIColor.green, name: "green"),
                    (color: UIColor.black, name: "black")]]
+    let textSizes = [[8, 12, 18, 28, 48]]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // picker view stuff
+        textColorPicker.delegate = self
+        textColorPicker.dataSource = self
+        textSizePicker.delegate = self
+        textSizePicker.dataSource = self
+        bgColorPicker.delegate = self
+        bgColorPicker.dataSource = self
+        
+        // textview stuff
+        // TODO: make keyboard close automatically
+        textView.delegate = self
+    }
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return colors.count
+        if pickerView == textColorPicker || pickerView == bgColorPicker {
+          return colors.count
+        }
+        else if pickerView == textSizePicker {
+            return textSizes.count
+        }
+        return 0
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return colors[component].count
+        if pickerView == textColorPicker || pickerView == bgColorPicker {
+            return colors[component].count
+        }
+        else if pickerView == textSizePicker {
+            return textSizes[component].count
+        }
+        return 0
+        
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return colors[component][row].name
+        if pickerView == textColorPicker || pickerView == bgColorPicker {
+            return colors[component][row].name
+        }
+        else if pickerView == textSizePicker {
+            return String(textSizes[component][row])
+        }
+        return ""
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
@@ -40,16 +75,9 @@ class ThemeEditViewController: UIViewController, UIPickerViewDelegate, UIPickerV
         else if pickerView == bgColorPicker {
             self.theme.bgColor = colors[component][row].color
         }
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        textColorPicker.delegate = self
-        textColorPicker.dataSource = self
-        textSizePicker.delegate = self
-        textSizePicker.dataSource = self
-        bgColorPicker.delegate = self
-        bgColorPicker.dataSource = self
+        else if pickerView == textSizePicker {
+            self.theme.textSize = textSizes[component][row]
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -57,15 +85,14 @@ class ThemeEditViewController: UIViewController, UIPickerViewDelegate, UIPickerV
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        if segue.identifier == "saveTheme" {
+            let mainView = segue.destination as! ViewController
+            mainView.theme = self.theme
+        }
     }
-    */
 
 }

ed864cf20e6a2155953ec37c7a331b240d3a23a8
Lucas Dachman <lucas.dachman@gmail.com>
2018-10-15 18:40:53 -0600

    first view (series resistors) on proj1 finished

---
+++ project-1/cirkit/cirkit/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  cirkit
+//
+//  Created by Lucas Dachman on 10/15/18.
+//  Copyright © 2018 Lucas Dachman. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ project-1/cirkit/cirkit/SeriesViewController.swift
@@ -0,0 +1,108 @@
+//
+//  FirstViewController.swift
+//  cirkit
+//
+//  Created by Lucas Dachman on 10/15/18.
+//  Copyright © 2018 Lucas Dachman. All rights reserved.
+//
+
+import UIKit
+
+class SeriesViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var r1Field: UITextField!
+    @IBOutlet weak var r2Field: UITextField!
+    @IBOutlet weak var r1Seg: UISegmentedControl!
+    @IBOutlet weak var r2Seg: UISegmentedControl!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        r1Field.delegate = self
+        r2Field.delegate = self
+        calculate()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func calculate() {
+        
+        
+        adjustForDecimal(r1Field)
+        adjustForDecimal(r2Field)
+        // ensure that both text fields have values
+        if let r1 = Float(r1Field.text!), let r2 = Float(r2Field.text!) {
+            if r1 >= 0 && r2 >= 0 {
+                let mult1 = getMultiplier(r1Seg.titleForSegment(at: r1Seg.selectedSegmentIndex))
+                let mult2 = getMultiplier(r2Seg.titleForSegment(at: r2Seg.selectedSegmentIndex))
+                var result:Float = (r1 * mult1) + (r2 * mult2)
+                if result >= 100000 {
+                    result /= 100000
+                    resultLabel.text = String(result) + "MΩ"
+                }
+                else if result >= 1000 {
+                    result /= 1000
+                    resultLabel.text = String(result) + "KΩ"
+                }
+                else {
+                    resultLabel.text = String(result) + "Ω"
+                }
+            } else {
+                resultLabel.text = "error"
+            }
+            
+        } else {
+            resultLabel.text = "–"
+        }
+        
+    }
+    
+    func adjustForDecimal(_ field: UITextField) {
+        if let val = field.text {
+            if val.hasPrefix(".") {
+                field.text = "0" + field.text!
+            }
+        }
+    }
+    
+    func getMultiplier(_ text: String?) -> Float {
+        if let option = text {
+            switch option {
+            case "Ω":
+                return 1
+            case "KΩ":
+                return 1000
+            case "MΩ":
+                return 100000
+            default:
+                return 1
+            }
+        }
+        return 1.0
+    }
+
+    @IBAction func editingChanged(_ sender: Any) {
+        calculate()
+    }
+    @IBAction func segValChanged(_ sender: Any) {
+        calculate()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>,
+                               with event: UIEvent?) {
+        self.view.endEditing(true)
+        calculate()
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        r1Field.resignFirstResponder()
+        r2Field.resignFirstResponder()
+        calculate()
+        return true
+    }
+
+    
+}
+

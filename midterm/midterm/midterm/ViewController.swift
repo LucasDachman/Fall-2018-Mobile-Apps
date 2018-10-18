90b89ff496fdcf4a8e1466f10acdeb730d6c4e74
Lucas Dachman <lucas.dachman@gmail.com>
2018-10-18 11:28:49 -0600

    part 1 finished

--- midterm/midterm/midterm/ViewController.swift
+++ midterm/midterm/midterm/ViewController.swift
@@ -8,13 +8,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var timeField: UITextField!
+    @IBOutlet weak var weekSwitch: UISwitch!
+    @IBOutlet weak var perWeekSlider: UISlider!
+    @IBOutlet weak var milesLabel: UILabel!
+    @IBOutlet weak var caloriesLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        timeField.delegate = self
+        calculate()
     }
 
+    @IBAction func onWorkoutTouched(_ sender: UIButton) {
+        calculate()
+    }
+    
+    func calculate() {
+        if let minutes = Float(timeField.text!) {
+            // calculate miles
+            let hours = (minutes / 60)
+            milesLabel.text = String(hours * 6) + " miles"
+            
+            //calculate calories
+            caloriesLabel.text = String(hours * 600) + " calories burned"
+            
+        } else {
+            milesLabel.text = "–"
+            caloriesLabel.text = "–"
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>,
+                               with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        timeField.resignFirstResponder()
+        return true
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.

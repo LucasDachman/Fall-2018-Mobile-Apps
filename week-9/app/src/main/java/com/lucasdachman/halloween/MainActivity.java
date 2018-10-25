952c9ca4963d830211687949972968ebaae09ba9
Lucas Dachman <lucas.dachman@gmail.com>
2018-10-25 11:48:00 -0600

    android intro

---
+++ week-9/app/src/main/java/com/lucasdachman/halloween/MainActivity.java
@@ -0,0 +1,23 @@
+package com.lucasdachman.halloween;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sayBoo(View view) {
+        EditText editText = findViewById(R.id.editText);
+        String name = editText.getText().toString();
+        TextView booText = findViewById(R.id.message);
+        booText.setText("Happy Halloween " + name + "!");
+    }
+}

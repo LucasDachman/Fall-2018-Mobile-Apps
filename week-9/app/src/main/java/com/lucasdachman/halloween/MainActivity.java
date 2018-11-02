1e22b0748dc9053407b08c8084b8751a17aa9f78
Lucas Dachman <lucas.dachman@gmail.com>
2018-11-02 12:39:21 -0600

    week 9

--- week-9/app/src/main/java/com/lucasdachman/halloween/MainActivity.java
+++ week-9/app/src/main/java/com/lucasdachman/halloween/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
@@ -19,5 +20,7 @@ public class MainActivity extends Activity {
         String name = editText.getText().toString();
         TextView booText = findViewById(R.id.message);
         booText.setText("Happy Halloween " + name + "!");
+        ImageView imageView = findViewById(R.id.imageView);
+        imageView.setImageResource(R.drawable.ghost);
     }
 }

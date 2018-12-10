2b173b3cac224133a68707442c1cd8d51bc3b159
Lucas Dachman <lucas.dachman@gmail.com>
2018-12-09 21:03:19 -0700

    fp spinner styles

--- project-2/ResistorCalculator/app/src/main/java/com/lucasdachman/resistorcalculator/ColorSpinnerAdapter.java
+++ project-2/ResistorCalculator/app/src/main/java/com/lucasdachman/resistorcalculator/ColorSpinnerAdapter.java
@@ -1,12 +1,15 @@
 package com.lucasdachman.resistorcalculator;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -20,11 +23,20 @@ public class ColorSpinnerAdapter extends ArrayAdapter<ResistorColor> {
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         return initView(position, convertView, parent);
+
     }
 
     @Override
     public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        return initView(position, convertView, parent);
+        ResistorColor colorItem = getItem(position);
+        TextView tv = new TextView(getContext());
+        tv.setText(colorItem.getName());
+        tv.setTextAppearance(getContext(), R.style.spinner_item);
+        tv.setBackgroundColor(colorItem.getColor());
+        int width = Resources.getSystem().getDisplayMetrics().widthPixels;
+        tv.setLayoutParams(new ViewGroup.LayoutParams(width, 100));
+
+        return tv;
     }
 
     private View initView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {

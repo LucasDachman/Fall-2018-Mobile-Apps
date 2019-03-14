d665f0f0f8811686da4dfda41477202991a1e439
Lucas Dachman <lucas.dachman@gmail.com>
2019-03-14 11:20:05 -0600

    midterm first commit

---
+++ MAD2-midterm/MAD2-midterm/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  MAD2-midterm
+//
+//  Created by Lucas Dachman on 3/14/19.
+//  Copyright © 2019 Lucas Dachman. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+

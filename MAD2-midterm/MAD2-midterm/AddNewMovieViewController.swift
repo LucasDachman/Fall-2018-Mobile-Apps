d23403272228869954ebae16c14c5a9fb2e37b61
Lucas Dachman <lucas.dachman@gmail.com>
2019-03-14 11:31:03 -0600

    nav probs works

---
+++ MAD2-midterm/MAD2-midterm/AddNewMovieViewController.swift
@@ -0,0 +1,38 @@
+//
+//  AddNewMovieViewController.swift
+//  MAD2-midterm
+//
+//  Created by Lucas Dachman on 3/14/19.
+//  Copyright © 2019 Lucas Dachman. All rights reserved.
+//
+
+import UIKit
+
+class AddNewMovieViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onCancel(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func onDone(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

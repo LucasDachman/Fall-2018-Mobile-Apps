ce52acaaaae6ef94f1669d0f037158a8f62af271
Lucas Dachman <lucas.dachman@gmail.com>
2019-05-04 14:22:03 -0600

    final part 1

---
+++ lucas-dachman-final/app/src/main/java/com/lucasdachman/lucas_dachman_final/MainActivity.java
@@ -0,0 +1,50 @@
+package com.lucasdachman.lucas_dachman_final;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Restaurant> restaurants;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        restaurants = new ArrayList<>();
+        restaurants.add(new Restaurant("Avery Brewing", "https://www.google.com"));
+        restaurants.add(new Restaurant("The Buff", "https://www.google.com"));
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.addItemDecoration(new DividerItemDecoration(recyclerView.getContext(), DividerItemDecoration.VERTICAL));
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        recyclerView.setAdapter(new Adapter(restaurants));
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
---
+++ lucas-dachman-final/app/src/main/java/com/lucasdachman/lucas_dachman_final/Restaurant.java
@@ -0,0 +1,10 @@
+package com.lucasdachman.lucas_dachman_final;
+
+public class Restaurant {
+    public String name;
+    public String url;
+    public Restaurant(String name, String url) {
+        this.name = name;
+        this.url = url;
+    }
+}
